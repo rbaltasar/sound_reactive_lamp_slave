92fb17905e279bd87fb47ecaa3a546c802cc7bdc
rbaltasar <r.baltasarb@gmail.com>
2019-06-17 16:24:57 +0200

    Update lamp_network_slave.ino

--- lamp_network_slave.ino
+++ lamp_network_slave.ino
@@ -12,35 +12,38 @@
 /* --- Global variables --- */
 /* ------------------------ */
 
+/* Web Server for OTA softare updates */
 OTAUpdater_ESP32 updater;
-
+/* Shared memory containing the global configuration and state of the lamp */
+/* This object is shared within the LED Controller and the Communication Handler */
 state_tracker<lamp_status> lamp_state;
-
+/* LED controller object */
 LEDController LED_controller(&lamp_state.val);
+/* Polymorphic pointer. Shall point to the currently used communication class */
+CommunicationHandler* communication_handler;
+/* MQTT communication handler */
 
-UDPHandler udp_handler(&lamp_state.val);
+/* UDP communication handler */
 
+/* Reseat reason. Only for debugging reasons. Remove long term */
 RESET_REASON reset_reason_0;
 RESET_REASON reset_reason_1;
-
-CommunicationHandler* communication_handler;
 
 void setup()
 {
   
-  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);
   delay(10);
 
   /* Get reset reason */
   get_reset_reason();
 
+  /* Setup the WiFi connection */
   setup_wifi();
-  delay(100);
+  /* Start with MQTT communication. Create object */  
   communication_handler = new MQTTHandler(&lamp_state.val);
-  delay(200);  
-  setup_hardware();  
-
-  lamp_state.val.sysState = STARTUP;
+  /* Setup the hardware */
+  setup_hardware(); 
 
   /* Initial configuration of the lamp when the system is booted */
   lamp_state.val.color.R = 0;
@@ -50,7 +53,9 @@ void setup()
   lamp_state.val.effect_delay = 50;
   lamp_state.val.effect_speed = 50; 
   lamp_state.val.effect_amount = 1;
+  lamp_state.val.sysState = STARTUP;
 
+  /* Same configuration for the "old" variable. TODO: create copy operator */
   lamp_state.old.color.R = 0;
   lamp_state.old.color.G = G_DEFAULT;
   lamp_state.old.color.B = 0;
@@ -59,7 +64,7 @@ void setup()
   lamp_state.old.effect_speed = 50; 
   lamp_state.old.effect_amount = 1;
 
-  /* Configuration state */
+  /* Init state */
   lamp_state.val.initState.hasStarted = false;
   lamp_state.val.initState.isCompleted = false;
 
@@ -67,6 +72,7 @@ void setup()
   LED_controller.setLeds(lamp_state.val.color,0,NUM_LEDS/3);
 }
 
+/* Get the IP address in String format */
 String IpAddress2String(const IPAddress& ipAddress)
 {
   return String(ipAddress[0]) + String(".") +\
@@ -75,6 +81,7 @@ String IpAddress2String(const IPAddress& ipAddress)
   String(ipAddress[3])  ;
 }
 
+/* Print reset reason. TODO: remove long term */
 void print_reset_reason(RESET_REASON reason)
 {
   switch ( reason)
@@ -97,7 +104,6 @@ void print_reset_reason(RESET_REASON reason)
     default : Serial.println ("NO_MEAN");
   }
 }
-
 void get_reset_reason()
 {
 
@@ -111,9 +117,10 @@ void get_reset_reason()
   print_reset_reason(reset_reason_0);
 }
 
+/* Setup WiFi connection */
 void setup_wifi()
 {
-  WiFi.begin(ssid, password); // Connect to the network
+  WiFi.begin(ssid, password); // Connect to the network. Use network_credentials.h
   Serial.print("Connecting to ");
   Serial.print(ssid);
   Serial.println(" ...");
@@ -134,24 +141,25 @@ void setup_wifi()
   Serial.print("MAC address: ");
   Serial.println(WiFi.macAddress());
 
+  /* Get MAC address */
   lamp_state.val.MACAddress_string = WiFi.macAddress();
 
   /* Translate the IP address to String to have a unique name for MQTT client */
   lamp_state.val.IPAddress_string = IpAddress2String(WiFi.localIP());  
-  
 }
 
+/* Setup the LED controller */
 void setup_hardware()
 {  
   LED_controller.setup();
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+/* Setup the OTA software update web server */
 void setup_OTA()
 {
   updater.begin(lamp_state.val.ota_url);
 }
-
 
 #if 0
 void streaming_loop()
@@ -181,10 +189,30 @@ void streaming_loop()
 }
 #endif
 
+/* Update the current lamp mode */
+/* Switch between communication handlers when necessary */
 void mode_update()
 {
   /* Finish previous effect */
   LED_controller.end_effect();
+  
+  /* Switch communication handlers if necessary */
+  if(lamp_state.val.lamp_mode >= 100 && lamp_state.old.lamp_mode < 100) //Switch from MQTT to UDP
+  {
+    /* Stop MQTT communication handler */
+    
+    /* Assign UDP communication handler */
+    
+    /* Start UDP communication handler */
+  }
+  else if(lamp_state.val.lamp_mode >= 100 && lamp_state.old.lamp_mode < 100) //Switch from UDP to MQTT
+  {
+    /* Stop UDP communication handler */
+    
+    /* Assign MQTT communication handler */
+    
+    /* Start MQTT communication handler */
+  }
   
   /* Streaming request */
   if(lamp_state.val.lamp_mode == 3)
@@ -198,24 +226,26 @@ void mode_update()
     lamp_state.val.lamp_mode = 2;
   }
 
-  /* Streaming request */
+  /* ON request - force default colors */
   if(lamp_state.val.lamp_mode == 1)
   {
     Serial.println("ON request received");
     lamp_state.val.color.R = R_DEFAULT;
     lamp_state.val.color.G = G_DEFAULT;
-    lamp_state.val.color.B = B_DEFAULT;
-    lamp_state.val.effect_delay = 50;
-    lamp_state.val.effect_speed = 50;   
+    lamp_state.val.color.B = B_DEFAULT;     
   }
   
   Serial.print("Received change request to mode ");
   Serial.println(lamp_state.val.lamp_mode);
-     
+  
+  /* Shadow current mode for evaluation of new requests */
   lamp_state.old.lamp_mode = lamp_state.val.lamp_mode; 
+  
+  /* Inform the LED controller about the update */
   LED_controller.update_mode();   
 }
 
+/* Check for update requests */
 void status_update()
 { 
   
@@ -225,41 +255,50 @@ void status_update()
     mode_update();
   }
 
+  /* Check difference in brightness request */
   else if(lamp_state.val.brightness != lamp_state.old.brightness)
   {
     Serial.print("Received change request to brightness level ");
     Serial.println(lamp_state.val.brightness);    
 
+    /* Shadow current change for evaluation of new requests */
     lamp_state.old.brightness = lamp_state.val.brightness;  
+    
+    /* Inform the LED controller about the update */
     LED_controller.update_mode();
   }
 
+  /* Check difference in color request */
   else if(lamp_state.val.color.R != lamp_state.old.color.R || lamp_state.val.color.G != lamp_state.old.color.G || lamp_state.val.color.B != lamp_state.old.color.B)
   {
     Serial.print("Received change request to color: ");
     Serial.println(lamp_state.val.color.R);
     Serial.println(lamp_state.val.color.G);   
-    Serial.println(lamp_state.val.color.B);    
+    Serial.println(lamp_state.val.color.B);  
 
-    lamp_state.val.brightness = lamp_state.val.brightness;  
-
-    LED_controller.update_mode();
-
+    /* Shadow current change for evaluation of new requests */
     lamp_state.old.color.R = lamp_state.val.color.R;
     lamp_state.old.color.G = lamp_state.val.color.G;
-    lamp_state.old.color.B = lamp_state.val.color.B;     
+    lamp_state.old.color.B = lamp_state.val.color.B;
+    
+    /* Inform the LED controller about the update */
+    LED_controller.update_mode();
   }
 }
 
+/* Handle initial communication handshake to ensurue stable MQTT communication with the broker */
 void initComm()
 {
   /* Send a MQTT request */
   if(!lamp_state.val.initState.hasStarted)
   {
+    /* Give visual feedback */
     LED_controller.setLeds(lamp_state.val.color,0,(NUM_LEDS*2)/3);
 
+    /* Initiate handshake. TODO: check that MQTT communication is being used */
     communication_handler->publish_initcomm();
     
+    /* Update handshake state */
     lamp_state.val.initState.hasStarted = true;
     lamp_state.val.initState.elapsed_time = millis();
   }
@@ -270,14 +309,18 @@ void initComm()
     /* Check if answer was received */
     if(lamp_state.val.initState.isCompleted)
     {
-
+      /* Finish the handshake */
       communication_handler->finish_initcomm();
       
+      /* Update system state. Go to normal mode */
       lamp_state.val.sysState = NORMAL;
+      
+      /* Begin OTA */
       setup_OTA();
 
-      delay(1000);
+      delay(500);
       
+      /* Give visual feedback */
       LED_controller.setAllLeds(lamp_state.val.color,0);
       
       lamp_state.val.color.R = R_DEFAULT;
@@ -292,33 +335,40 @@ void initComm()
     /* Timeout. Show error and reset */
     else if( (millis() - lamp_state.val.initState.elapsed_time) > lamp_state.val.initState.timeout )
     {
+      Serial.println("Error in communication setup. Restarting ESP32");
+      
+      /* Give visual feedback */
       lamp_state.val.color.R = R_DEFAULT;
       lamp_state.val.color.G = 0;
       lamp_state.val.color.B = 0;
       LED_controller.setAllLeds(lamp_state.val.color,0);
-
-      Serial.println("Error in communication setup. Restarting ESP32");
-
       delay(1000);
 
+      /* Restart microcontroller */
       ESP.restart();
     }
   }
 }
 
+/* Main loop */
 void loop()
 {
+  /* Execute the network loop of the currently used communication handler */
   communication_handler->network_loop();
 
   switch(lamp_state.val.sysState)
   {
-    case STARTUP:      
+    case STARTUP:
+      /* Handle initial communication handshake */
       initComm();
       break;
       
-    case NORMAL:     
+    case NORMAL: 
+      /* Handle OTA */
       updater.OTA_handle();
+      /* Check for requested updates */
       status_update();
+      /* Feed the LED controller */
       LED_controller.feed();   
       break;
   }
