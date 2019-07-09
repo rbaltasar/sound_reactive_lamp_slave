a6472c36cf7315f90aeb88d186c017233d926c35
rbaltasar <r.baltasarb@gmail.com>
2019-07-09 17:35:13 +0200

    Update LED_Music_Effects.cpp

--- LED_Music_Effects.cpp
+++ LED_Music_Effects.cpp
@@ -5,33 +5,36 @@
 LEDMusicEffects::LEDMusicEffects(LEDController* led_controller, CRGB* leds_ptr, timeSync* timer):
 m_led_controller(led_controller),
 m_leds(leds_ptr),
-m_timer(timer)
-{
-  //start_sequence = true;
-  m_last_iteration = 0;
+m_timer(timer),
+m_last_iteration(0)
+{  
 }
 
 LEDMusicEffects::~LEDMusicEffects()
 {
-  //end_effect();
+  end_effect();
 }
 
+/* Finish the current effect and cleanup */
 void LEDMusicEffects::end_effect()
 {
-  //start_sequence = true;
-  
+  /* TBD: any action needed? */  
 }
 
+/* Syncrhonize the effects with other slaves 
+*  This relies on receiving the UDP sync request
+*/
 void LEDMusicEffects::resync()
 {
   m_last_iteration = m_timer->getTime();
 }
 
+/* Color conversion from RGB to HSV */
 LEDMusicEffects::hsv LEDMusicEffects::rgb2hsv(RGBcolor in_rgb)
 {
 
     hsv         in;
-    hsv         out;
+    hsv         out; //Re-use of hsv structure to avoid redefining another structure with similar content only for this
     double      min, max, delta;
 
     /* Adapt format */
@@ -78,11 +81,12 @@ LEDMusicEffects::hsv LEDMusicEffects::rgb2hsv(RGBcolor in_rgb)
     return out;
 }
 
+/* Color conversion from HSV to RGB */
 RGBcolor LEDMusicEffects::hsv2rgb(LEDMusicEffects::hsv in)
 {
   double      hh, p, q, t, ff;
   long        i;
-  hsv         out; //same datatype to buffer the output
+  hsv         out; //Re-use of hsv structure to avoid redefining another structure with similar content only for this
   RGBcolor retVal;
 
     if(in.s <= 0.0) {       // < is bogus, just shuts up warnings
@@ -147,46 +151,51 @@ RGBcolor LEDMusicEffects::hsv2rgb(LEDMusicEffects::hsv in)
     return retVal;     
 }
 
-void LEDMusicEffects::shift_leds(uint8_t led_start, uint8_t led_end, uint8_t positions, const bool top, const uint8_t delay_ms, const uint8_t R_in, const uint8_t G_in, const uint8_t B_in)
+/* Shift leds a defined number of positions with a given input color */
+void LEDMusicEffects::shift_leds(
+  uint8_t led_start,  //start position (always < led_end)
+  uint8_t led_end,  //end position
+  uint8_t positions,  //number of leds to shift
+  const bool top,  //shift direction true-->upwards, false-->downwards
+  const uint8_t delay_ms, //delay (in ms) after shifting the leds. TODO: Needed?
+  const uint8_t R_in, //input color
+  const uint8_t G_in, //input color
+  const uint8_t B_in  //input color
+)
 {
-  if(led_end <= led_start) return;
-  
+  /* Prevent wrong order */
+  if(led_end <= led_start) return;  
+  /* Minimum number of positions to shift must be 1 */
   if(positions == 0) positions = 1;
+  /* Clip the number of leds to shift */
   if( (led_end - led_start) < positions ) positions = led_end - led_start;
   
+  /* Upwards direction */
   if(top)
   {
+    /* Shift existing colors */
     for(uint8_t j = led_end - 1; j >= (led_start + positions) ; j--)
     {
-      m_leds[j] = m_leds[j-positions];
-      //Serial.print("Position shift up: ");
-      //Serial.println(j);
-      //delay(50);
+      m_leds[j] = m_leds[j-positions];     
     }
+    /* Introduce input color */
     for(uint8_t j = led_start; j < (led_start + positions); j++)
     {
       m_leds[j] = CRGB(R_in,G_in,B_in);
-      //Serial.print("Position fill up: ");
-      //Serial.println(j);
-      //delay(50);
     }
   }
-
+  /* Downwards direction */
   else 
   {
+    /* Shift existing colors */
     for(uint8_t j = led_start; j < (led_end - positions); j++)
     {
       m_leds[j] = m_leds[j+positions];
-      //Serial.print("Position shift down: ");
-      //Serial.println(j);
-      //delay(50);
     }
+    /* Introduce input color */
     for(uint8_t j = led_end - positions; j < led_end; j++)
     {
       m_leds[j] = CRGB(R_in,G_in,B_in);
-      //Serial.print("Position fill down: ");
-      //Serial.println(j);
-      //delay(50);
     }
   }
 
@@ -194,40 +203,36 @@ void LEDMusicEffects::shift_leds(uint8_t led_start, uint8_t led_end, uint8_t pos
   delay(delay_ms);
 }
 
+/* Translate from a relative amplitude [0-100] to an absolute suitable amplitude for bubble effect */
 uint8_t LEDMusicEffects::compute_bubble_amplitude(uint8_t amplitude)
 {
   uint8_t adaptedAmplitude;
   
-  if(amplitude > 75) adaptedAmplitude = 5;
-  else if(amplitude > 50) adaptedAmplitude = 4;
-  else if(amplitude > 40) adaptedAmplitude = 3;
-  else if(amplitude > 20) adaptedAmplitude = 2;
+  if(amplitude > 75) adaptedAmplitude = 4;
+  else if(amplitude > 50) adaptedAmplitude = 3;
+  else if(amplitude > 40) adaptedAmplitude = 2;
   else adaptedAmplitude = 1;
 
   return adaptedAmplitude;
 }
 
-void LEDMusicEffects::bubble_effect(uint32_t print_delay, uint8_t r, uint8_t g, uint8_t b, uint8_t amplitude, uint8_t direction)
+/* Bubble effect */
+void LEDMusicEffects::bubble_effect(
+  uint32_t refresh_rate, //effect refresh rate
+  uint8_t r, //new color
+  uint8_t g, //new color
+  uint8_t b, //new color
+  uint8_t amplitude, //effect amplitude. Range [0-100]
+  uint8_t direction //effect direction
+)
 {
   unsigned long now = m_timer->getTime();
 
-  if( (now - m_last_iteration) > print_delay )
+  if( (now - m_last_iteration) > refresh_rate )
   {
-#if 0
-    Serial.println("Running bubble effect. ");
-    Serial.print("Color: ");
-    Serial.print(r);
-    Serial.print(" ");
-    Serial.print(g);
-    Serial.print(" ");
-    Serial.println(b);
-    Serial.print("Dir: ");
-    Serial.println(direction);
-#endif
-
     m_last_iteration = now;
 
-    /* Adapt the amplitude from a range [0-100] to a number of leds to shift */
+    /* Adapt the amplitude for this effect */
     amplitude = compute_bubble_amplitude(amplitude);
     
     if(direction == 0) //Up 
@@ -246,42 +251,61 @@ void LEDMusicEffects::bubble_effect(uint32_t print_delay, uint8_t r, uint8_t g, 
     else if(direction == 3) //Out-middle
     {
       shift_leds(0, NUM_LEDS / 2 , amplitude, true, 0, r, g, b);
-      shift_leds(NUM_LEDS / 2, NUM_LEDS, amplitude, false, 0, r, g, b);
-      
+      shift_leds(NUM_LEDS / 2, NUM_LEDS, amplitude, false, 0, r, g, b);      
     }
   }
 }
 
-void LEDMusicEffects::print_amplitude_color(uint8_t led_start, uint8_t led_end, const bool top, uint8_t amplitude, uint8_t r, uint8_t g, uint8_t b)
+/* Print a number of leds with a given color */
+void LEDMusicEffects::print_amplitude_color(
+  uint8_t led_start, //start position (must be < led_end)
+  uint8_t led_end, //end position
+  const bool top, //effect direction
+  uint8_t amplitude, //number of leds to print
+  uint8_t r, //color
+  uint8_t g, //color
+  uint8_t b  //color
+)
 {
-  
+  /* Upwards direction */
   if(top)
-  {    
+  { 
+    /* Print requested amplitude */
     for(uint8_t i = led_start; i < led_start + amplitude; i++)
     {
       m_leds[i] = CRGB(r,g,b);
     }
+    /* Erase rest */
     for(uint8_t i = led_start + amplitude; i < led_end; i++)
     {
       m_leds[i] = CRGB(0,0,0);
     }
   }
+  /* Downwards direction */
   else
   {
+    /* Print requested amplitude */
+    for(uint8_t i = (led_end - amplitude); i < led_end; i++)
+    {
+      m_leds[i] = CRGB(r,g,b);
+    } 
+    /* Erase rest */
     for(uint8_t i = led_start; i < (led_end - amplitude); i++)
     {
       m_leds[i] = CRGB(0,0,0);
     }
-    for(uint8_t i = (led_end - amplitude); i < led_end; i++)
-    {
-      m_leds[i] = CRGB(r,g,b);
-    }    
+       
   }
    FastLED.show();  
 }
 
-
-void LEDMusicEffects::generate_static_colors(uint8_t r_base, uint8_t g_base, uint8_t b_base, uint8_t increment)
+/* Generate a list of colors based on a given base color and a Hue increment */
+void LEDMusicEffects::generate_static_colors(
+  uint8_t r_base, //base color
+  uint8_t g_base, //base color
+  uint8_t b_base, //base color
+  uint8_t increment //Increment step (in degrees) for each single generated color
+)
 {
   
   hsv hsv_color;
@@ -290,34 +314,9 @@ void LEDMusicEffects::generate_static_colors(uint8_t r_base, uint8_t g_base, uin
   rgb_color.R = r_base;
   rgb_color.G = g_base;
   rgb_color.B = b_base;
-
-#if 0
-  Serial.print("Color base: ");
-  Serial.print(r_base);
-  Serial.print(" ");
-  Serial.print(g_base);
-  Serial.print(" ");
-  Serial.println(b_base);
-
-  Serial.print("Increment: ");
-  Serial.println(increment);
-#endif
   
   /* Translate the base color from RGB to HSV */
   hsv_color = rgb2hsv(rgb_color);
-
-#if 0
-  Serial.print("HSV base: ");
-  Serial.print(hsv_color.h);
-  Serial.print(" ");
-  Serial.print(hsv_color.s);
-  Serial.print(" ");
-  Serial.println(hsv_color.v);
-#endif
-
-  /* Set S and V to the maximum value */
-  //hsv_color.s = 1.0;
-  //hsv_color.v = 1.0;
 
   /* Increment the Hue for each led */
   for(uint8_t i = 0; i < NUM_LEDS; i++)
@@ -326,53 +325,56 @@ void LEDMusicEffects::generate_static_colors(uint8_t r_base, uint8_t g_base, uin
     if(hsv_color.h > 360.0) hsv_color.h = 0.0; //Circular increment between [0,360]
     /* Convert to RGB */
     rgb_color = hsv2rgb(hsv_color);
-
-#if 0
-    Serial.print("RGB[");
-    Serial.print(i);
-    Serial.print("]: ");
-    Serial.print(rgb_color.R);
-    Serial.print(" ");
-    Serial.print(rgb_color.G);
-    Serial.print(" ");
-    Serial.println(rgb_color.B);
-#endif
-    
+   
     /* Add color to the led shadow memory */
     m_static_color[i] = CRGB(rgb_color.R,rgb_color.G,rgb_color.B);
-  }
-  
+  }  
 }
 
+/* Check if any of the input parameters have been updated since the last iteration/function call */
 bool LEDMusicEffects::is_update(uint8_t r, uint8_t g, uint8_t b, uint8_t amplitude)
 {
   bool retVal = false;
   
+  /* Get last iteration values from object generic memory */
   uint8_t r_old = music_effect_mem[0];
   uint8_t g_old = music_effect_mem[1];
   uint8_t b_old = music_effect_mem[2];
   uint8_t ampl_req = music_effect_mem[3];
   
+  /* Check for updates in color */
   if( (r_old != r) || (g_old != g) || (b_old != b) )
   {
     /* Update effect memory */
     music_effect_mem[0] = r;
     music_effect_mem[1] = g;
-    music_effect_mem[2] = b;
-    
+    music_effect_mem[2] = b;    
     retVal = true;
   } 
+  /* Check for updates in amplitude (step ignored when generic amplitude 0xFF given) */
   else if( (amplitude != 0xFF) && (ampl_req != amplitude) )
   {
-    music_effect_mem[3] = amplitude;
-    
+    /* Update effect memory */
+    music_effect_mem[3] = amplitude;    
     retVal = true;
   }
   
   return retVal;
 }
 
-void LEDMusicEffects::print_amplitude_static(uint8_t led_start, uint8_t led_end, const bool top, uint8_t amplitude, uint8_t r_base, uint8_t g_base, uint8_t b_base, uint8_t increment)
+/* Print a number of leds with a given base color and amplitude
+*  When the given base color is changed, a new static color list will be generated
+*/
+void LEDMusicEffects::print_amplitude_static(
+  uint8_t led_start, //start position (must be < led_end)
+  uint8_t led_end,  //end position
+  const bool top, //effect direction
+  uint8_t amplitude,  //number of leds to print
+  uint8_t r_base,  //base color
+  uint8_t g_base,  //base color
+  uint8_t b_base,  //base color
+  uint8_t increment //unit increment for new color list generation
+)
 {
   
   /* Check if the base color has been changed  */
@@ -383,59 +385,57 @@ void LEDMusicEffects::print_amplitude_static(uint8_t led_start, uint8_t led_end,
      generate_static_colors(r_base,g_base,b_base,increment);
   }
  
+  /* Upwards direction */
   if(top)
   {  
-   
+    /* Print requested amplitude from generated color */
     for(uint8_t i = led_start; i < led_start + amplitude; i++)
     {
       m_leds[i] = m_static_color[i];
     }
+    /* Erase rest */
     for(uint8_t i = led_start + amplitude; i < led_end; i++)
     {
       m_leds[i] = CRGB(0,0,0);
     }
   }
   else
-  { 
-    for(uint8_t i = led_start; i < (led_end - amplitude); i++)
-    {
-      m_leds[i] = CRGB(0,0,0);
-    }
+  {     
+    /* Print requested amplitude from generated color */
     for(uint8_t i = (led_end - amplitude); i < led_end; i++)
     {
       m_leds[i] = m_static_color[i];
-    }    
+    } 
+    /* Erase rest */
+    for(uint8_t i = led_start; i < (led_end - amplitude); i++)
+    {
+      m_leds[i] = CRGB(0,0,0);
+    }       
   }
    FastLED.show();  
 } 
   
-
-void LEDMusicEffects::power_bars_effect(uint32_t print_delay, uint8_t r, uint8_t g, uint8_t b, uint8_t& amplitude, uint8_t direction, uint8_t effect_type, uint8_t increment)
+/* Power bars effect */
+void LEDMusicEffects::power_bars_effect(
+  uint32_t refresh_rate, //effect refresh rate
+  uint8_t r, //effect color
+  uint8_t g, //effect color
+  uint8_t b, //effect color
+  uint8_t& amplitude, //number of leds to print. This value can be modified by the function
+  uint8_t direction, //effect direction (top, down, middle-out, out-middle)
+  uint8_t effect_type, //effect type (static color, given color)
+  uint8_t increment //color unit increment (only for static color type)
+)
 {
   unsigned long now = m_timer->getTime();
 
   /* Time to update the LED status */
-  if( (now - m_last_iteration) > print_delay )
-  {
-    #if 0
-    Serial.println("Running power bars effect. ");
-    Serial.print("Color: ");
-    Serial.print(r);
-    Serial.print(" ");
-    Serial.print(g);
-    Serial.print(" ");
-    Serial.println(b);
-    Serial.print("Requested amplitude: ");
-    Serial.println(amplitude);
-    Serial.print("Dir: ");
-    Serial.print(direction);
-    Serial.print(" Effect ");
-    Serial.println(effect_type);
-    #endif
-    
+  if( (now - m_last_iteration) > refresh_rate )
+  {    
     /* Reset timer */
     m_last_iteration = now;
 
+    /* Given color effect */
     if(effect_type == 0)
     {
       if(direction == 0) //Up
@@ -457,6 +457,7 @@ void LEDMusicEffects::power_bars_effect(uint32_t print_delay, uint8_t r, uint8_t
         print_amplitude_color(0, NUM_LEDS / 2, true , amplitude / 2, r, g, b);
       }
     }
+    /* Static color effect */
     else if(effect_type == 1)
     {
       if(direction == 0) //Up
@@ -480,6 +481,7 @@ void LEDMusicEffects::power_bars_effect(uint32_t print_delay, uint8_t r, uint8_t
     }
 
     /* If there is no update since last iteration, perform a decay effect by reducing the requested amplitude one unit */
+    /* This helps to make the effect smoother in case of high network load, where no new UDP packets are received between iterations */
     if( !is_update(r,g,b,amplitude) )
     {
       if(amplitude != 0) amplitude--;     
